dec156318642fe7d3c153021f4f40bac6ebc3b69
chiefdinkan <rahulrex123456789@gmail.com>
2024-06-07 23:06:19 +0530

    idk it is nesscesary or not

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
-use tokio::task;
 use std::path::PathBuf;
+use tokio::task;
 
 mod help;
 mod read;
 
 #[tokio::main]
 
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = help::help_args();
     let color_code = args.color_code;
     let files: Vec<PathBuf> = args.files;
 
-    let tasks = files.iter().map(|file| {
-        let file = file.clone();
-        let color_code = color_code.clone();
-        task::spawn(async move {
-            if let Err(e) = read::read_file(file.clone(), color_code.clone()).await {
-                eprintln!("Error reading {:?}: {}", file, e);
-            }
+    let tasks: Vec<_> = files
+        .into_iter()
+        .map(|file| {
+            let color_code = color_code.clone();
+            task::spawn(async move {
+                if let Err(e) = read::read_file(file.clone(), color_code.clone()).await {
+                    eprintln!("Error reading {:?}: {}", file, e);
+                }
+            })
         })
-    });
-    let handles: Vec<_> = tasks.collect();
+        .collect();
 
-    for handle in handles {
+    for handle in tasks {
         if let Err(e) = handle.await {
             eprintln!("Task failed: {:?}", e);
         }
     }
+
+    Ok(())
 }
